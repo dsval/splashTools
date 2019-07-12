94e4cceffa19190e4a4749ac20624b9fac4c3202
dsval <dsandovalhdh@gmail.com>
2019-07-12 17:43:31 +0100

    calculate wtd

--- R/unsSWC.R
+++ R/unsSWC.R
@@ -1,6 +1,6 @@
 #' unsSWC
 #'
-#' Estimates water content using an analytca integral of the profile described by Brooks and Corey 1964
+#' Estimates water content using an analytca integral of the profile described by Brooks and Corey 1964 and calculates depth to water table if there is any
 #' @param  soil_data, vector retrieved from getSoilNRCS() or getSoilSite()
 #' @param  uns_depth, depth to where to calculate water content in (m)
 #' @param  wn, xts object, total water content of the profile from surface to bedrock (m)
@@ -42,6 +42,7 @@ unSWC<-function(soil_data,uns_depth,wn){
 	}
 	
 	# wn<-simdf[,1]
+	
 	soil_info<-soil_hydro(sand=soil_data[1],clay=soil_data[2],OM=soil_data[3],fgravel =soil_data[4] ,bd = soil_data[5])
 	SAT<-soil_info$SAT*(1-soil_data[4]/100)*soil_data[6]*1000
 	RES<-soil_info$RES*(1-soil_data[4]/100)*soil_data[6]*1000
@@ -51,9 +52,16 @@ unSWC<-function(soil_data,uns_depth,wn){
 	lambda<-as.numeric(1/soil_info$B)
 	bub_press<-as.numeric(soil_info$bubbling_p)
 	theta_i<-wn/(soil_data[6]*1000)
+	theta_i[theta_i>=theta_s]<-theta_s-0.05
+	theta_i[theta_i<=theta_r]<-theta_r+0.01
 	psi_m = bub_press/((((theta_i-theta_r)/(theta_s-theta_r)))^(1/lambda));
 	psi_m<-as.numeric(psi_m)
 	uns_wn<-UnsWater(psi_m,z_uns,theta_r,theta_s,bub_press,lambda)
 	uns_theta<-uns_wn/(uns_depth*1000)
-	xts(uns_theta,time(wn))
+	uns_theta<-xts(uns_theta,time(wn))
+	uns_wn<-xts(uns_wn,time(wn))
+	wtd<-(bub_press-psi_m)/1000
+	wtd[wtd>soil_data[6]]<-soil_data[6]
+	merge.xts(uns_wn,wtd)
+	
 }
